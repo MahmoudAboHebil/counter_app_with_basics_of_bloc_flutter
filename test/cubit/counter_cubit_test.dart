e98dcdb26769daa3f9cac4932966d2cdce28f9b3
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-28 15:37:29 +0300

    Adding the testing part to test the counter_cubit.dart.

---
+++ test/cubit/counter_cubit_test.dart
@@ -0,0 +1,35 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:counter_app_with_basics_of_bloc/cubit/counter_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CounterCubit', () {
+    late CounterCubit counterCubit;
+    setUp(() {
+      counterCubit = CounterCubit();
+    });
+
+    tearDown(() {
+      counterCubit.close();
+    });
+
+    test(
+        'the initial state for the CounterCubit is counterState(counterValue:0)',
+        () {
+      expect(counterCubit.state, CounterState(counterValue: 0));
+    });
+
+    blocTest(
+      'the cubit should emit a CounterState(counterValue:1,wasIncremented:true) when cubit.increment() is called',
+      build: () => counterCubit,
+      act: (cubit) => cubit.increment(),
+      expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+    blocTest(
+      'the cubit should emit a CounterState(counterValue:-1,wasIncremented:false) when cubit.decrement() is called',
+      build: () => counterCubit,
+      act: (cubit) => cubit.decrement(),
+      expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+    );
+  });
+}
