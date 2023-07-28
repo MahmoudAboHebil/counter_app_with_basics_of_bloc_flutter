ff02e5cbbc3bd9cd6629d07f39ba66358e01de94
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-28 15:37:14 +0300

    Adding the testing part to test the counter_cubit.dart.

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,10 +1,14 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
-  int counterValue;
-  bool? wasIncremented;
+class CounterState extends Equatable {
+  final int counterValue;
+  final bool? wasIncremented;
   CounterState({
     required this.counterValue,
     this.wasIncremented,
   });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [counterValue, wasIncremented];
 }
